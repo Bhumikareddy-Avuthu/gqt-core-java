f366e49520952a9cd9789b358df0bbddd9022212
sweety <sweety@192.168.1.16>
2025-07-25 12:17:53 +0530

    loaded all the pattern programs

---
+++ src/com/gqt/corejava/pattern/Pattern19.java
@@ -0,0 +1,17 @@
+package com.gqt.corejava.pattern;
+import java.util.Scanner;
+public class Pattern19 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the count:");
+		int n=sc.nextInt();
+		int count=1;
+		for(int i=1;i<=n;i++) {
+			for(int j=i;j<=n;j++) {
+				System.out.print(count+" ");
+				count++;
+			}
+            System.out.println();
+	}
+}
+}
